83c80290ef202febbd7b7f7f09f8e42a63d817e2
Steven5678231 <53145087+Steven5678231@users.noreply.github.com>
2021-03-02 14:21:19 +1100

    update store init state

--- src/store.js
+++ src/store.js
@@ -8,7 +8,23 @@ const persistedState = localStorage.getItem("reduxState")
       rooms: [],
       video: true,
       audio: true,
-      controlParams: {},
+      controlParams: {
+        occlusion_mask: false, //Switch
+        feature_show: {
+          eyes: {
+            toggle: true,
+            sliderIndex: 0,
+          },
+          mouth: {
+            toggle: true,
+            sliderIndex: 0,
+          },
+          nose: {
+            toggle: true,
+            sliderIndex: 0,
+          },
+        },
+      },
     };
 const store = createStore(reducers, persistedState);
 store.subscribe(mapStoreToStorage);
