3579aa140f257bd4978499f930039446065fc1fa
Steven5678231 <53145087+Steven5678231@users.noreply.github.com>
2021-05-25 09:52:37 +1000

    Add routers

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var router = express.Router();
+var path = require("path");
+
+/* GET SURVEY PAGE. */
+
+router.get("/", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "..", "/dist/index.html"));
+});
+
+module.exports = router;
---
+++ routes/projection.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var router = express.Router();
+var path = require("path");
+
+/* GET SURVEY PAGE. */
+
+router.get("/", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "..", "/public/projection/template.html"));
+});
+
+module.exports = router;
