962b74f57f2f78edae5b52a8653ac4189390e85b
Hao <53145087+Steven5678231@users.noreply.github.com>
2021-06-29 15:30:17 +1000

    update consent form

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -1,14 +1,27 @@
 import React, { useEffect, useState } from "react";
+import Checkbox from "@material-ui/core/Checkbox";
 
 function Announcement(props) {
-  const [rating, setRating] = useState("general");
-  const onSubmit = props.handler(rating);
+  const [rating, setRating] = useState("mature");
+  const [checked, setCheck] = useState(false);
+  function onSubmit() {
+    props.handler(rating);
+  }
+  const handleCheck = (event) => {
+    setCheck(event.target.checked);
+    if (event.target.checked && rating != "kids") {
+      setRating("kids");
+    }
+    if (!event.target.checked && rating != "mature") {
+      setRating("mature");
+    }
+  };
   return (
     <div
       style={{
         display: "flex",
         flexDirection: "column",
-        background: "black",
+        // background: "black",
         zIndex: 100,
         height: "100%",
         width: "100%",
@@ -16,12 +29,21 @@ function Announcement(props) {
       }}
     >
       <div>
-        <h>Introduction</h>
+        <h>Consent Form</h>
         <p>This experience will go for 5 minutes broken into 3 sections</p>
         <p>
           During each section you will receive a talking prompt and a survey
           question to answer on this ipad
         </p>
+        <input type="checkbox" onChange={handleCheck} />
+        <span class="checkboxtext"> I am under 18 years old</span>
+        {/* <Checkbox
+          checked={checked}
+          color="white"
+          onChange={handleCheck}
+          inputProps={{ "aria-label": "primary checkbox" }}
+          label="I am under 18 years old"
+        /> */}
       </div>
 
       <button onClick={onSubmit}>{props.stage}</button>
